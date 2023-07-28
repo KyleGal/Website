cfed1f5972e4c9afff7e4943325e15e60c500d77
KyleGal <107329064+KyleGal@users.noreply.github.com>
2023-07-28 10:35:37 -0700

    new icons and loading screen

--- js/loadingScreen.js
+++ js/loadingScreen.js
@@ -1,23 +1,31 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loading = document.querySelector(".progColor");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const loading = document.querySelector(".progColor");
 
-    loading.addEventListener("animationend", () => {
-        const loadingScreen = document.querySelector("#loadingScreenDisplay");
-        console.log(loadingScreen);
+//     loading.addEventListener("animationend", () => {
+//         const loadingScreen = document.querySelector("#loadingScreenDisplay");
+//         console.log(loadingScreen);
         
-        loadingScreen.style.transition = "0.2s";
-        loadingScreen.style.opacity = "0";
+//         loadingScreen.style.transition = "0.2s";
+//         loadingScreen.style.opacity = "0";
 
-        loadingScreen.addEventListener("transitionend", () => {
-            var websiteScreen = document.getElementById("websiteDisplay");
-            loadingScreen.style.display = "none";
+//         loadingScreen.addEventListener("transitionend", () => {
+//             var websiteScreen = document.getElementById("websiteDisplay");
+//             loadingScreen.style.display = "none";
 
-            websiteScreen.style.transition = "opacity 0.5s";
-            websiteScreen.style.opacity = "100";
-            // makes sure we start at the top
-            window.scrollTo(0,0);
-        });
-    });
-});
+//             websiteScreen.style.transition = "opacity 0.5s";
+//             websiteScreen.style.opacity = "100";
+//             // makes sure we start at the top
+//             window.scrollTo(0,0);
+//         });
+//     });
+// });
+
+window.onload = function() {
+    const loadingScreen = document.querySelector('.loadingScreen');
+    loadingScreen.style.display = 'none';
+
+    const body = document.body;
+    body.style.overflow = 'auto';
+};
 
